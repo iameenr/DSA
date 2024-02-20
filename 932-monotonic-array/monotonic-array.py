0560f2c4ac0943bdc730481fd920b272468268a7
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-02-20 19:46:23 +0530

    Time: 713 ms (79.93%) | Memory: 31.7 MB (41.84%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -2,16 +2,19 @@ class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
 
         inc, dec = None, None
+        lennums = len(nums)
 
         prev = nums[0]
-        for n in nums[1:]:
+        for i in range(1, lennums):
+            n = nums[i]
             if n < prev:
                 dec = True
                 break
             prev = n
 
         prev = nums[0]
-        for n in nums[1:]:
+        for i in range(1, lennums):
+            n = nums[i]
             if n > prev:
                 inc = True
                 break
