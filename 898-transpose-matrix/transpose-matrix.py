55f951667c201cd23c71853a80ffe8aa054bcdbf
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-02-21 21:24:23 +0530

    Time: 68 ms (55.10%) | Memory: 17.2 MB (92.15%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,9 +1,10 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        result_matrix = [[0] * len(matrix) for _ in range(len(matrix[0]))]
+        rowllen, collen = len(matrix), len(matrix[0])
+        result_matrix = [[0] * rowllen for _ in range(collen)]
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+        for r in range(rowllen):
+            for c in range(collen):
                 result_matrix[c][r] = matrix[r][c]
         
         return result_matrix
