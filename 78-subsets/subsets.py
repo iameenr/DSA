10268e93372fc5598cbcac29bb2a5094856da3a9
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-02-27 22:40:25 +0530

    Time: 31 ms (91.40%) | Memory: 16.7 MB (56.34%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def _subsets(ls):
-            if not ls:
+        def _subsets(index):
+            if index == numslen:
                 yield []
             else:
-                # first = ls[0]
-                # rest  = ls[1:]
-                for subset in _subsets(ls[1:]):
+                # first = [nums[index]]
+                # rest = index + 1
+                for subset in _subsets(index + 1):
                     yield subset
-                    yield [ls[0]] + subset  
-
-        return [subset for subset in _subsets(nums)]
+                    yield [nums[index]] + subset
+        
+        numslen = len(nums)
+        return [subset for subset in _subsets(0)]
 
 
