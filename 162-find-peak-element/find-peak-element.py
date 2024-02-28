d01c2e5123ebbbb32a13633b3f840d82421153a2
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-02-28 08:10:30 +0530

    Time: 53 ms (21.32%) | Memory: 16.6 MB (91.63%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,7 +1,20 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-
         lennums = len(nums)-1
-        for i, n in enumerate(nums):
-            if (i == 0 or nums[i-1] < nums[i])  and (i == lennums or nums[i] > nums[i+1]):
-                return i  
+        low, high = 0, lennums
+
+        while low <= high:
+
+            mid = (low+high)//2
+
+            if (mid == 0 or nums[mid-1] < nums[mid]) and \
+               (mid == lennums or nums[mid] > nums[mid+1]):
+               return mid
+            # if on increasing curve, search right:
+            elif mid < lennums and nums[mid] < nums[mid+1]:
+                low = mid+1
+            # On decreasing curve, so search right:
+            else:
+                high = mid-1
+
+        return -1
