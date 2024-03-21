fb4e926c91c72246e1559494f79b733ac9f946fe
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-03-21 07:21:21 +0530

    Time: 39 ms (97.56%) | Memory: 16.6 MB (85.49%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        def combination(i, target, seq):
+            if target == 0:
+                yield seq
+            else:
+                n = candidates[i]
+                if n <= target:
+                    # use this index again
+                    yield from combination(i, target - n, seq + [n])
+
+                    # skip to next index
+                    if i+1 < len(candidates):
+                        yield from combination(i+1, target, seq)
+            
+    
+        return [c for c in combination(0, target, [])]
