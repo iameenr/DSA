3b441b6237f8372e010441a777c7c0f83aecb2e9
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-05-06 21:53:22 +0530

    Time: 37 ms (51.95%) | Memory: 16.4 MB (81.82%) - LeetSync

---
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countVowelStrings(self, n):
+        
+        a, e, i, o, u = 1, 1, 1, 1, 1
+
+        while n > 1:
+            o += u
+            i += o
+            e += i
+            a += e
+            n -= 1
+
+        return a + e + i + o + u
