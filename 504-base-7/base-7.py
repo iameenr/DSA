eb09d2b384a6b4343317743ae50146625a69f126
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-08-28 00:06:11 +0530

    Time: 30 ms (86.81%) | Memory: 16.5 MB (43.13%) - LeetSync

--- 504-base-7/base-7.py
+++ 504-base-7/base-7.py
@@ -1,19 +1,18 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num == 0:
-            return "0"
+        if num == 0: return "0"
         
         IS_NEGATIVE = num < 0
         num = abs(num)
         res = []
-
+        
         while num != 0:
             # remainder = num % 7
             res.append(str(num % 7))
-            num //= 7
+            num = num // 7 
+
 
         if IS_NEGATIVE:
             res.append('-')
 
-        # reverse a list ::-1
         return "".join(res[::-1])
