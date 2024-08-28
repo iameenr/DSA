5c357aa7ed155117acd476010c30a11157755e8c
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-08-28 22:44:18 +0530

    Time: 806 ms (21.93%) | Memory: 56.1 MB (12.27%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,21 +1,17 @@
-import collections
-
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        # Base case: if string is empty or k is greater than the string length, return 0
         if not s or k > len(s):
             return 0
 
-        # Count the frequency of each character in the string
-        freq = collections.Counter(s)
+        frequency = collections.Counter(s)
 
-        # Iterate through the string
         for i, char in enumerate(s):
             # If the character's frequency is less than k, split the string
-            if freq[char] < k:
+            if frequency[char] < k:
                 left_part = self.longestSubstring(s[:i], k)
                 right_part = self.longestSubstring(s[i+1:], k)
                 return max(left_part, right_part)
 
-        # If no character with frequency less than k was found, return the length of the string
+        # If no character with frequency less than k was found, 
+        # return the length of the string
         return len(s)
