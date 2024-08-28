c78a555958cafa88236a796d8ccc264841e12e00
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-08-28 22:57:51 +0530

    Time: 223 ms (6.20%) | Memory: 30.4 MB (21.44%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,20 +1,25 @@
-import math
-
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        window_sum = 0
         min_window_length = math.inf
+        window_sum = 0
         start = 0
-        
-        for end, n in enumerate(nums):
-            window_sum += n
 
-            # shrink from left
+        end = 0
+        while end in range(len(nums)):
+            window_sum += nums[end]
+
             while window_sum >= target:
+                # then shrink the window from left
+                # current_wl = (end - start) + 1
                 min_window_length = min(min_window_length, (end - start) + 1)
+
+                # shrinking
                 window_sum -= nums[start]
                 start += 1
+            
+            end += 1
 
-        if min_window_length == math.inf: 
+        if min_window_length == math.inf:
             return 0
         return min_window_length
+            
