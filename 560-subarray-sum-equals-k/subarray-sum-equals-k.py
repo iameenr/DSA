192dc43ad8676498f4810e6b1600d0fed3334cbf
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-09-06 22:35:17 +0530

    Time: 229 ms (56.46%) | Memory: 19.6 MB (24.65%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        
         prefix_sum = 0
-        prefix_sum_dict = {0 : 1}
-
-        result = 0
+        prefix_sum_dict = {0 : 1} # prefix_sum : num of times it was seen
+        times = 0
+        
         for n in nums:
-            prefix_sum = prefix_sum + n
-            want = prefix_sum - k
-            
-            if want in prefix_sum_dict:
-                result += prefix_sum_dict[want]
+            prefix_sum += n
+
+            target = prefix_sum - k
+            if target in prefix_sum_dict:
+                times = times + prefix_sum_dict[target]
 
             if prefix_sum not in prefix_sum_dict:
                 prefix_sum_dict[prefix_sum] = 0
-                
             prefix_sum_dict[prefix_sum] += 1
 
-        return result
+        return times
