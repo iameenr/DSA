a452e7f97c113e522d222e6d1de8308344a9d0e4
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-09-11 22:58:49 +0530

    Time: 36 ms (45.40%) | Memory: 16.6 MB (14.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,47 @@
+"""
+n steps
+1 or 2 steps - at a time
+
+number of permutations to climb to top
+
+"""
+
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n in [0,1,2]: return n
+        # memo = {}
+        # def ways(n):
+        #     if n in [0, 1, 2]:
+        #         return n
+        #     if n in memo:
+        #         return memo[n]
 
-        one = 1
-        two = 2
-        for i in range(3, n+1):
-            curr = one + two
-            one = two
-            two = curr
-        return curr
+        #     memo[n] = ways(n-1) + ways(n-2)
+        #     return memo[n]
+
+        if n == 1: return 1
+        if n == 2: return 2
+
+        dp = [0]*(n+1)   
+        dp[0], dp[1], dp[2] = 0, 1, 2
+
+        for s in range(3, n+1):
+            dp[s] = dp[s-1] + dp[s-2]
+
+        return dp[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
