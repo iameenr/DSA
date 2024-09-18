cb1daca7286cfd29d2ce248667dacf66e18cb6d8
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-09-18 23:54:29 +0530

    Time: 30 ms (86.91%) | Memory: 16.6 MB (8.39%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
+        def combine(index, combination):
+            if index == lendigits:
+                yield "".join(combination)
+            else:
+                for char in digits_to_letters[digits[index]]:
+                    yield from combine(index+1, combination + [char])
+            
+
+        if digits == "": 
+            return ""
+
         digits_to_letters = {
             '2': 'abc',
             '3': 'def',
@@ -10,22 +20,7 @@ class Solution:
             '7': 'pqrs',
             '8': 'tuv',
             '9': 'wxyz',
-         }
-
-        def combine(index, combination):
-            if index == end:
-                combinations.append("".join(combination))
-                return
-
-            # digit = digits[index]
-            for c in digits_to_letters[digits[index]]:
-                combine(index+1, combination + [c])
-            
-
-        if digits == "": 
-            return []
-
-        end = len(digits)
-        combinations = []
-        combine(0, [])
-        return combinations
+        }
+        lendigits = len(digits)
+        return [c for c in combine(0, [])]
+        
