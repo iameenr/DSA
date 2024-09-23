821a0db03276835470ecc698dc8729d28c6aab60
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-09-24 00:44:19 +0530

    Time: 30 ms (83.44%) | Memory: 16.5 MB (14.35%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,10 +3,11 @@ class Solution:
         if(n == 1): return 1
         if(n == 2): return 2
 
-        first=1; second=2; current=0
+        first, second, current = 1, 2, 0
         for steps in range(3,n+1):
-            current = first+second
+            current = first + second
             first = second
             second = current
+            
         return current
         
