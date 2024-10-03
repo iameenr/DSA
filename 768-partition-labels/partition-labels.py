5a850608c0f868441203fd0d3b915d8bcb7ac640
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-10-03 22:37:27 +0530

    Time: 34 ms (91.31%) | Memory: 16.6 MB (34.14%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,43 +1,51 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        """merge intervals"""
         
-        fld = {} # c : [foc, loc] | a : [0, 9]
-        LOC = 1
-        seen = set()
-        for i, c in enumerate(s):
-            if c in seen:
-                # next occurence
-                fld[c][LOC] = i
-            else:
-                # fld[c][FOC], fld[c][LOC] = i, i
-                fld[c] = [i, i]
-                seen.add(c)
+        def get_first_last_occurence(s):
+            first_last_dict = {} # c : [foc, loc] | a : [0, 9]
+            LOC = 1
+            seen = set()
+            for i, c in enumerate(s):
+                if c in seen:
+                    # next occurence
+                    first_last_dict[c][LOC] = i
+                else:
+                    # first_last_dict[c][FOC], first_last_dict[c][LOC] = i, i
+                    first_last_dict[c] = [i, i]
+                    seen.add(c)
+
+            return first_last_dict
+
+        def merge_overlapping_intervals(intervals):
+            # merge_overlapping intervals
+            intervals.sort(key=lambda x:x[0])
 
 
-        intlist = []
-        for k, v in fld.items():
-            intlist.append(v)
+            lenintervals = len(intervals)   
 
-        # merge_overlapping intervals
-        intlist.sort(key=lambda x:x[0])
+            merged_intervals = [intervals[0]]
+            for i in range(1, lenintervals):
+                cs, ce = intervals[i]
+                le = merged_intervals[-1][1]
 
+                if le > cs: #overlap
+                    merged_intervals[-1][1] = max(le, ce)
+                else:
+                    merged_intervals.append([cs, ce])
 
-        lenintervals = len(intlist)   
+            return merged_intervals
 
-        result = [intlist[0]]
-        for i in range(1, lenintervals):
-            cs, ce = intlist[i]
-            le = result[-1][1]
+        first_last_dict = get_first_last_occurence(s)
 
-            if le > cs: #overlap
-                result[-1][1] = max(le, ce)
-            else:
-                result.append([cs, ce])
+        intervals = []
+        for k, v in first_last_dict.items():
+            intervals.append(v)
+
+        merged_intervals = merge_overlapping_intervals(intervals)
 
         # take sum and return 
         partitions = []
-        for s, e in result:
+        for s, e in merged_intervals:
             partitions.append((e-s)+1)
 
         return partitions
