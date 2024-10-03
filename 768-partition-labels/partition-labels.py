6dd06b92584bae851d2806856b673888a0d1a0a4
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-10-03 23:04:27 +0530

    Time: 33 ms (93.10%) | Memory: 16.7 MB (5.93%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -2,46 +2,24 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
         def get_last_occurence(s):
-            last_occurence = {} # c : loc like a : 9
+            last_occurence = {}  # Store the last occurrence of each character
             for i, c in enumerate(s):
                 last_occurence[c] = i
-
             return last_occurence
 
-        def merge_overlapping_intervals(intervals):
-            # merge_overlapping intervals
-            intervals.sort(key=lambda x:x[0])
-
-
-            lenintervals = len(intervals)   
-
-            merged_intervals = [intervals[0]]
-            for i in range(1, lenintervals):
-                cs, ce = intervals[i]
-                le = merged_intervals[-1][1]
-
-                if le > cs: #overlap
-                    merged_intervals[-1][1] = max(le, ce)
-                else:
-                    merged_intervals.append([cs, ce])
-
-            return merged_intervals
-
         last_occurence = get_last_occurence(s)
+        start, end = 0, last_occurence[s[0]]
+        lens = len(s)
+        partitions = []  
 
-        intervals = []
-        seen = set()
-        for i, c in enumerate(s):
-            if c not in seen:
-                seen.add(c)
-                intervals.append([i, last_occurence[c]])
-
-
-        merged_intervals = merge_overlapping_intervals(intervals)
-
-        # take sum and return 
-        partitions = []
-        for s, e in merged_intervals:
-            partitions.append((e-s)+1)
+        while start < lens:
+            i = start
+            end = last_occurence[s[start]]  
+    
+            while i <= end:
+                end = max(end, last_occurence[s[i]])  # Extend the partition 
+                i += 1
+            partitions.append((end - start) + 1)  
+            start = end + 1
 
         return partitions
