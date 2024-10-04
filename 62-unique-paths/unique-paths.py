0dc651d5e5367154ae4524dd331b2811e19c6df3
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-10-04 22:09:25 +0530

    Time: 35 ms (58.89%) | Memory: 16.5 MB (42.92%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,25 +1,12 @@
 class Solution:
     def uniquePaths(self, rowlen: int, collen: int) -> int:
+        """ 
+        """     
+        dp = [[1]*collen for _ in range(rowlen)]
+
+        for c in range(collen-2, -1, -1):
+            for r in range(rowlen-2, -1, -1):
+                dp[r][c] = dp[r+1][c] + dp[r][c+1]
+
         
-        def dfs(r, c):
-            if r == rowlen - 1 and c == collen - 1:
-                return 1
-            
-            if (r, c) in memo: 
-                return memo[(r, c)]
-
-            paths = 0
-            # Move right if within bounds
-            if r + 1 < rowlen:
-                paths += dfs(r + 1, c)
-            
-            # Move down if within bounds
-            if c + 1 < collen:
-                paths += dfs(r, c + 1)
-
-            memo[(r, c)] = paths
-            return paths
-
-        memo = {}
-        paths = dfs(0, 0)
-        return paths
+        return dp[0][0]
