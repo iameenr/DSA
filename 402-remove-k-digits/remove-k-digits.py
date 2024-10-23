1831da63279a78e82cb06f9526dd18ee7251661d
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-10-23 22:14:18 +0530

    Time: 23 ms (97.31%) | Memory: 17.6 MB (96.77%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -1,24 +1,27 @@
+from collections import deque
+
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        if not num:
-            return ""
-            
-        stack = []
-        stack.append(num[0])
+        if not num: 
+            return "0"
         
-        for i in range(1, len(num)):
-            digit = num[i]
-            
-            while stack and stack[-1] > digit and k:
-                stack.pop() # remove peak
+        stack = deque()
+        TOP = -1
+
+        for n in num:
+            while k > 0 and stack and stack[TOP] > n:
+                stack.pop()
                 k -= 1
-            
-            stack.append(digit)
-        
-        while k:
-            stack.pop() # now we just want to remove any additional elements from the end
+            stack.append(n)
+
+        while k > 0:
+            stack.pop()
             k -= 1
-        
-        res = "".join(stack).lstrip("0")
-        
-        return res if res != "" else "0"
+
+        # Remove trailing zeroes from left
+        while stack and stack[0] == '0':
+            stack.popleft()
+
+        result = ''.join(stack)
+
+        return result if result else "0"
