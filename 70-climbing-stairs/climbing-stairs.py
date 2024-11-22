d6ede5b0427b483e56b7139d5dd969a8275953fc
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2024-11-22 22:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (23.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,28 +1,12 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 3:
-            return n 
+    def climbStairs(self, num_steps: int) -> int:
+        if num_steps <= 3:
+            return num_steps
 
         first, second = 1, 2
-        for _ in range(3, n+1):
+        for current in range(3, num_steps+1):
             current = first + second
             first = second
             second = current
 
         return current
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
