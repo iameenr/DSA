078feb764d5bee20239632502aa079fea4ae113a
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2025-07-10 00:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,45 @@
 class Solution:
-    def climbStairs(self, num_steps: int) -> int:
-        if num_steps <= 3:
-            return num_steps
+    def climbStairs(self, n: int) -> int:
+        # def _climb(n, memo):
+        #     if n <= 3:
+        #         return n
 
-        first, second = 1, 2
-        for current in range(3, num_steps+1):
-            current = first + second
-            first = second
-            second = current
+        #     if n in memo:
+        #         return memo[n]
 
-        return current
+        #     take_one_step  = self.climbStairs(n-1)
+        #     take_two_steps = self.climbStairs(n-2)
+        #     memo[n] = take_one_step + take_two_steps
+        #     return memo[n]
+
+        # memo = {} # 4 : 5 ways
+        # return _climb(n, memo)
+
+        if n <= 3:
+            return n
+
+        # assign base case
+        dp = [0 for _ in range(n+1)]
+        dp[0], dp[1], dp[2], dp[3] = 0, 1, 2, 3
+
+
+        # rewrite recurrence as dp array
+        # f(n) = f(n-1) + f(n-2)
+        for i in range(4, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
