83d36957e84d7a540d02fd175d45f44591e9ac4c
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2025-07-11 02:31:37 +0530

    Time: 10 ms (22.88%) | Memory: 25.7 MB (21.04%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,17 +5,39 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
         n = len(nums)
-        k = k % n  # This handles cases where k is larger than n
-        
-        # If k is 0, no changes are needed
-        if k == 0:
-            return
+        k %= n  # Handle cases where k > n
 
-        temp = nums[-k:]  
-        
-        # Shift the rest of the elements to the right by k positions
-        for i in range(n-1, k-1, -1):
-            nums[i] = nums[i-k]
+        def reverse(start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
 
-        nums[:k] = temp
+        # 1: Reverse the entire array
+        reverse(0, n - 1)
+
+        # 2: Reverse the first k elements
+        reverse(0, k - 1)
+
+        # 3: Reverse the rest
+        reverse(k, n - 1)
+
+
+
+        """O(k)"""
+        # n = len(nums)
+        # k = k % n  # This handles cases where k is larger than n
+        
+        # # If k is 0, no changes are needed
+        # if k == 0:
+        #     return
+
+        # temp = nums[-k:]  
+        
+        # # Shift the rest of the elements to the right by k positions
+        # for i in range(n-1, k-1, -1):
+        #     nums[i] = nums[i-k]
+
+        # nums[:k] = temp
