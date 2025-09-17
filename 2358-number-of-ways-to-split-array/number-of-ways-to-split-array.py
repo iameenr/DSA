0b10c2658ea46b450420244ec4135e6fc9d5c54b
Ameenur Rahman <43576567+iameenr@users.noreply.github.com>
2025-09-17 22:44:45 +0530

    Time: 34 ms (99.48%) | Memory: 32.4 MB (72.68%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -3,14 +3,12 @@ class Solution:
         valid_splits = 0 # result
         lennums = len(nums)
 
-        total_sum = 0
-        for n in nums:
-            total_sum += n
+        total_sum = sum(nums)
         
         prefix_sum = 0
         for i in range(0, lennums - 1):
             prefix_sum += nums[i]
-            # sum_of_rest = (total_sum - prefix_sum)
+            # suffix_sum = (total_sum - prefix_sum)
             if prefix_sum >= (total_sum - prefix_sum):
                 valid_splits += 1
 
