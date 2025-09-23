09e2c25757487721c46ff89fab323017f58ea2b1
Ameen <43576567+iameenr@users.noreply.github.com>
2025-09-23 09:35:56 +0530

    Time: 106 ms (87.16%) | Memory: 58.2 MB (7.18%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -3,7 +3,6 @@ import random
 class RandomizedSet:
     def __init__(self):
         self.values = []
-        self.last_index = -1
         self.value_index_map = {}
 
     def insert(self, val: int) -> bool:
@@ -11,24 +10,30 @@ class RandomizedSet:
             return False
 
         self.values.append(val)
-        self.last_index += 1
-        self.value_index_map[val] = self.last_index
+        self.value_index_map[val] = len(self.values) - 1
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.value_index_map:
             return False
 
+        # removal_index = self.value_index_map[val]
+        # last_value = self.values[-1]
+        # self.values[removal_index] = last_value
+        # self.value_index_map[last_value] = removal_index
+        #
+        # self.values.pop()
+        # del self.value_index_map[val]
+
         removal_index = self.value_index_map[val]
         del self.value_index_map[val]
 
-        if removal_index != self.last_index:
-            last_value = self.values[self.last_index] # replace with last value
+        if removal_index != (len(self.values) - 1):
+            last_value = self.values[-1] # replace with last value
             self.values[removal_index] = last_value
             self.value_index_map[last_value] = removal_index
-        
+
         self.values.pop()
-        self.last_index -= 1
         return True
 
 
