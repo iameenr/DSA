500a45acbbcabbf448e808c9bffa14b7e20729de
Ameen <43576567+iameenr@users.noreply.github.com>
2025-09-27 23:48:22 +0530

    Time: 34 ms (31.94%) | Memory: 23.4 MB (44.09%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,27 +1,23 @@
 class Solution:
-  def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
-    total_tank = 0
-    current_tank = 0
-    start_station = 0
-    
-    for i in range(len(gas)):
-      # Update total and current tank balances with the net gain/loss at station i.
-      net_cost = gas[i] - cost[i]
-      total_tank += net_cost
-      current_tank += net_cost
-      
-      # If current_tank is negative, it means we can't reach station i+1
-      # from the current start_station.
-      if current_tank < 0:
-        # The new potential starting point is the next station.
-        start_station = i + 1
-        # Reset the tank for the new journey segment.
-        current_tank = 0
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        ts = len(gas) - 1
+        tg = tc = 0
+        for i in range(ts+1):
+            tg += gas[i]
+            tc += cost[i]
+        if tg < tc: # not possible
+            return -1
+
+        tnc = 0
+        start_idx = 0
+        for i in range(ts+1):
+            nc = gas[i] - cost[i]
+            tnc += nc
+
+            if tnc < 0:
+                # reset
+                tnc = 0
+                start_idx = i+1
+
+        return start_idx
         
-    # If total_tank is non-negative, a solution exists.
-    # The start_station found is the correct one.
-    # Otherwise it's impossible to complete the circuit.
-    if total_tank >= 0:
-        return start_station
-    else:
-        return -1
