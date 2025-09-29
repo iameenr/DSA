fd6e7f07bc41d3dbe0ca767d36333148e09cfbc3
Ameen <43576567+iameenr@users.noreply.github.com>
2025-09-29 23:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.88%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,17 +1,34 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """one pass solution"""
         dummy = ListNode(0, head)
-        lenofll = 0
-        node = head
+        first = sec = dummy
 
-        while node:
-            lenofll += 1
-            node = node.next
+        for _ in range(n+1):
+            first = first.next
+        
+        while first:
+            first = first.next
+            sec = sec.next
+        
+        sec.next = sec.next.next
 
-        ridx = lenofll - n
-        node = dummy
-        for _ in range(ridx):
-            node = node.next
-            
-        node.next = node.next.next
         return dummy.next
+
+
+"""two pass soln"""
+# dummy = ListNode(0, head)
+# lenofll = 0
+# node = head
+
+# while node:
+#     lenofll += 1
+#     node = node.next
+
+# ridx = lenofll - n
+# node = dummy
+# for _ in range(ridx):
+#     node = node.next
+
+# node.next = node.next.next
+# return dummy.next
