050af878f8e363d5127a7248f49bd5262f7e7ad2
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-05 16:40:24 +0530

    Time: 46 ms (23.23%) | Memory: 18.6 MB (70.14%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,16 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-
-        lennums = len(nums)
         
-        max_reach = 0
-        for index in range(lennums):
-            if index > max_reach:
+        maxreach = 0
+        for idx, jump in enumerate(nums):
+            if idx > maxreach:
                 return False
-            
-            reach = index + nums[index]
-            max_reach = max(max_reach, reach)
-            if max_reach >= (lennums - 1):
-                return True
+            maxreach = max(maxreach, (idx + jump))
 
-        return False
+        return True
