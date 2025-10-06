6fa39582c56b14150bcbe8f823e8d2cdd0637b94
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-07 00:58:29 +0530

    Time: 36 ms (0.00%) | Memory: 33.5 MB (5.64%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.py
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.py
@@ -1,15 +1,23 @@
 class Solution:
     def longestSubsequence(self, nums: List[int]) -> int:
-        n = len(nums)
-        xor_all = 0
-        for x in nums:
-            xor_all ^= x
         
-        if xor_all != 0:
-            return n
+        # intuition:
+        # - if whole array XORs to nonzero, return n
+        # - if whole array XORs to zero, 
+        #     then removing any 1 element will make xorsum non-zero,
+        #     we can return n-1.
+        #     but the removed element must not be zero.
 
-        # xor_all == 0
-        if all(x == 0 for x in nums):
-            return 0
-            
-        return n - 1
+        lenn = len(nums)
+        xorsum = 0
+        for n in nums:
+            xorsum = xorsum ^ n
+        
+        if xorsum != 0:
+            return lenn
+
+        for n in nums:
+            if n != 0:
+                return lenn-1
+                
+        return 0
