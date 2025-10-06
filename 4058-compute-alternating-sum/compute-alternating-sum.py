003a0be0b071978f2928b08a9ccdbb679bb40177
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-06 23:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (21.01%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.py
+++ 4058-compute-alternating-sum/compute-alternating-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def alternatingSum(self, nums: List[int]) -> int:
+        def altsum(index):
+            altsum_ = 0
+            for i in range(index, lennums + 1, 2):
+                altsum_ += nums[i]
+            return altsum_
+
         lennums = len(nums) - 1
-
-        asum = 0
-        for i in range(0, lennums + 1, 2):
-            asum += nums[i]
-        
-        ssum = 0
-        for i in range(1, lennums+1, 2):
-            ssum += nums[i]
-
-        return asum - ssum
+        return altsum(0) - altsum(1)
