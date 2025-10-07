c6e3c53b6227db554695402bb8de135666bb8384
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-08 00:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (98.91%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,29 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        lennums = len(nums)
-        def _get_subsets(index):
-            if index >= lennums:
-                yield []
-                return 
+        def backtrack(start_index, path):
+            result.append(list(path))
+            
+            for i in range(start_index, len(nums)):
+                path.append(nums[i])     
+                backtrack(i + 1, path)   
+                path.pop()                
 
-            first = nums[index]
-            # rest = nums[index:]
-            for subset in _get_subsets(index + 1):
-                yield subset
-                yield [first] + subset
+        result = []       
+        backtrack(0, [])
+        return result
 
-        return list(_get_subsets(0))
+
+        # def _get_subsets(index):
+        #     if index >= lennums:
+        #         yield []
+        #         return 
+
+        #     first = nums[index]
+        #     for subset in _get_subsets(index + 1):
+        #         yield subset
+        #         yield [first] + subset
+
+        # lennums = len(nums)
+        # return list(_get_subsets(0))
 
         
