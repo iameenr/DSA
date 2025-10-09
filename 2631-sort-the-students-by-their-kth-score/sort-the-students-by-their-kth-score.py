851ef13d0b37858b039432f7a9d24b44daa46155
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-09 14:20:01 +0530

    Time: 3 ms (43.96%) | Memory: 22.7 MB (54.47%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
         # Sort each student's scores by the kth score in descending order
-        sorted_scores = sorted(score, key=lambda student: student[k], reverse=True)
-        return sorted_scores
+        # sorted_scores = sorted(score, key=lambda student: student[k], reverse=True)
+        # return sorted_scores
+        return sorted(score, key=lambda student: student[k], reverse=True)
