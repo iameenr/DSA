8b55716c557d35b043ebffafab8d15377cedd930
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-13 23:51:06 +0530

    Time: 613 ms (19.33%) | Memory: 32.2 MB (19.75%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,6 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        current_xor = reduce(xor, nums)
+        current_xor = 0
+        for num in nums:
+            current_xor ^= num
+
         # xors with target k to find differing bits
         diff = current_xor ^ k
         
