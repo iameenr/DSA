35d58f6ff014fd75237ba667aac67e8729853700
Ameen <43576567+iameenr@users.noreply.github.com>
2025-10-14 23:56:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (92.49%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,15 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def _generateParenthesis(open_count, close_count, paran):
+        def _generateParenthesis(curr_comb, open_count, close_count):
             if open_count == n and close_count == n:
-                well_formed_paranthesis.append("".join(paran))
+                result.append(curr_comb)
                 return
-
             if open_count < n:
-                _generateParenthesis(open_count+1, close_count, paran + ['('])
-            if close_count < open_count and close_count < n:
-                _generateParenthesis(open_count, close_count+1, paran + [')'])
-            
-        well_formed_paranthesis = []
-        _generateParenthesis(0, 0, [])
-        return well_formed_paranthesis
+                _generateParenthesis(curr_comb + "(", open_count + 1, close_count)
+            if close_count < n and open_count > close_count:
+                _generateParenthesis(curr_comb + ")", open_count, close_count + 1)
+
+        result = []
+        _generateParenthesis("", 0, 0)
+        return result
