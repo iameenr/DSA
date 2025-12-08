d3dae32a99f46781a45f2c3de8707a8e24068dd8
Ameen <43576567+iameenr@users.noreply.github.com>
2025-12-09 00:01:43 +0530

    Time: 35 ms (18.41%) | Memory: 18 MB (77.56%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
+        permutations = [[]]
+        if not nums:
+            return permutations
 
-        if nums is None or len(nums) == 0:
-            return res
-
-        for num in nums:
-            new_res = []
-            for perm in res:
-                for i in range(len(perm) + 1):
-                    new_perm = perm[:i] + [num] + perm[i:]
-                    if new_perm not in new_res:  # Check for uniqueness
-                        new_res.append(new_perm)
-
-            res = new_res
-
-        return res
+        for number in nums:
+            updated_permutations = []
+            for permutation in permutations:
+                for index in range(len(permutation) + 1):
+                    candidate = permutation[:index] + [number] + permutation[index:]
+                    if candidate not in updated_permutations:
+                        updated_permutations.append(candidate)
+            permutations = updated_permutations
+            
+        return permutations
